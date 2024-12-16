f580576e86237d43812c2c7bc26fa0516d51d129
Felix Maier <fmai@allix.ch>
2024-12-16 13:10:23 +0100

    starting out, and I already realized that I am trash

---
+++ AdventOfCode2024/AdventOfCode2024/Day1/Day1.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2024.Day1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Days
+{
+	public class Day1
+	{
+		public int CompareLists()
+		{
+		Input day1Input = new Input();
+			day1Input.CreateList();
+            for (int i = 0; i < day1Input.list1.Count; i++)
+            {
+				int num1 = day1Input.list1.IndexOf(i);
+				int num2 = day1Input.list2.IndexOf(i);
+				
+            }
+
+
+
+
+		}
+
+
+
+	}
+}
---
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2024.Day1;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AdventOfCode2024
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+		}
+	}
+}
