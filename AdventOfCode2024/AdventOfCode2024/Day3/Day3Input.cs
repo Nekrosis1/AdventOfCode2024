76cc587716579dd2a04a501d762229150fe4bd2e
Felix Maier <f.maier@rafisa.ch>
2024-12-19 17:55:37 +0100

    But what if I dont regex, can I LINQ my way out of this?

---
+++ AdventOfCode2024/AdventOfCode2024/Day3/Day3Input.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day3
+{
+    public class Day3Input
+    {
+        public string GetDay3Input()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "inputs", "input3test.txt");
+            string input = File.ReadAllText(filePath);
+            return input;
+        }
+    }
+}
+
---
+++ AdventOfCode2024/AdventOfCode2024/Day3/Day3Logic.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace AdventOfCode2024.Day3
+{
+    public class Day3Logic
+    {
+        public int findMul()
+        {
+        Day3Input day3Input = new();
+        string input = day3Input.GetDay3Input();
+            
+        }
+    }
+}
